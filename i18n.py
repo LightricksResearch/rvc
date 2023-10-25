3607a76004488ace57e0607c212ba8e3d334b63a
Amit Pintz <apintz@lightricks.com>
2023-10-25 11:41:22 +0300

    i18n: Update relative paths to base dir.

--- i18n.py
+++ i18n.py
@@ -1,10 +1,13 @@
 import locale
 import json
+import logging
 import os
 
+from pathlib import Path
 
-def load_language_list(language):
-    with open(f"./lib/i18n/{language}.json", "r", encoding="utf-8") as f:
+
+def load_language_list(language, language_file_path):
+    with open(language_file_path, "r", encoding="utf-8") as f:
         language_list = json.load(f)
     return language_list
 
@@ -15,11 +18,13 @@ class I18nAuto:
             language = locale.getdefaultlocale()[
                 0
             ]  # getlocale can't identify the system's language ((None, None))
-        if not os.path.exists(f"./lib/i18n/{language}.json"):
+        base_dir = Path(__file__).parent.resolve()
+        language_file_path = os.path.join(base_dir, f"lib/i18n/{language}.json")
+        if not os.path.exists(language_file_path):
+            logging.info(f"No match was found for {language}, fallback to en_US")
             language = "en_US"
         self.language = language
-        # print("Use Language:", language)
-        self.language_map = load_language_list(language)
+        self.language_map = load_language_list(language, language_file_path)
 
     def __call__(self, key):
         return self.language_map.get(key, key)
